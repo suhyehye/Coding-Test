1d33df59515f1770404cc57171dd658e97450241
조수혜 <99079272+suhyehye@users.noreply.github.com>
2022-12-11 15:19:02 +0900

    Create 기능개발.py

---
+++ 프로그래머스/level2/기능개발.py
@@ -0,0 +1,13 @@
+import math
+def solution(progresses, speeds):
+    a=[math.ceil((100-progresses[p])/speeds[p]) for p in range(len(progresses))]
+    answer = [1]
+    big = a[0]
+    
+    for i in range(1,len(a)):
+        if big >= a[i]:
+            answer[-1] = answer[-1]+1
+        else:
+            big = a[i]
+            answer.append(1)
+    return answer
