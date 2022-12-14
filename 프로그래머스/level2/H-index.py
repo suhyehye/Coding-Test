c4176102d1829f282cb304141fa014b9df461d1a
조수혜 <99079272+suhyehye@users.noreply.github.com>
2022-12-14 10:09:07 +0900

    Update H-index.py

--- 프로그래머스/level2/H-index.py
+++ 프로그래머스/level2/H-index.py
@@ -1,5 +1,6 @@
 def solution(citations):
     for i in range(len(citations),-1, -1):
         new = len(list(filter(lambda x: x >=i, citations)))
+        
         if new >= i and len(citations) - new <= i:
             return i
