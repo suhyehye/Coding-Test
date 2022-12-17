76a69606fbb8e6bd8d22218e8adc1922b7e45f68
조수혜 <99079272+suhyehye@users.noreply.github.com>
2022-12-17 22:27:38 +0900

    Create 햄버거만들기.py

---
+++ 프로그래머스/level1/햄버거만들기.py
@@ -0,0 +1,10 @@
+def solution(ingredient):
+    answer = 0
+    hamburger = []
+    for i in ingredient:
+        hamburger.append(i)
+        
+        if hamburger[-4:] == [1,2,3,1]:
+            answer += 1
+            del hamburger[-4:]
+    return answer
