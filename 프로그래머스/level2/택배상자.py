f075443c7f5975d14e14ede8c80f480c2a2242de
조수혜 <99079272+suhyehye@users.noreply.github.com>
2022-12-26 22:30:58 +0900

    Update 택배상자.py

--- 프로그래머스/level2/택배상자.py
+++ 프로그래머스/level2/택배상자.py
@@ -1,19 +1,17 @@
 def solution(order):
     answer = 0
-    box = list(range(0,order[0]+1))
-    container = [0]
+    box = list(range(1,order[0]+1))
     for idx,i in enumerate(order):
+        if len(box) == 0:
+            box.append(i)
         if i == box[-1]:
             answer += 1
-            container.append(i)
             box.pop()
         elif i == order[idx-1]+1:
-            container.append(i)
             answer += 1
         elif i < box[-1]:
             return answer
         else:
             answer += 1
-            box.extend(list(range(max(order[:idx-1])+1, i)))
-    print(container)
+            box.extend(list(range(max(order[:idx])+1, i)))
     return answer
