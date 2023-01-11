fb8fb862ab530dbf38c0144868ab8896316e96eb
조수혜 <99079272+suhyehye@users.noreply.github.com>
2023-01-11 22:16:25 +0900

    Update 줄서는방법.py

--- 프로그래머스/level2/줄서는방법.py
+++ 프로그래머스/level2/줄서는방법.py
@@ -2,11 +2,10 @@ from math import factorial
 def solution(n, k):
     answer = []
     waiting = list(range(1,n+1))
-    fac = factorial(n)
     k -= 1
     while n >= 1:
-        fac = int(fac / n)
         n -= 1
+        fac = factorial(n)
         ans = waiting.pop(k//fac)
         answer.append(ans)
         k = k%fac
