2929cb057d641c2db25e3e52c613a127898c34ae
조수혜 <99079272+suhyehye@users.noreply.github.com>
2023-01-19 21:43:42 +0900

    Update 주식가격.py

--- 프로그래머스/level2/주식가격.py
+++ 프로그래머스/level2/주식가격.py
@@ -1,4 +1,17 @@
+from collections import deque
 def solution(prices):
     answer = []
-    prices > 1
+    prices = deque(prices)
+    n_prices = len(prices)
+    
+    for i in range(n_prices-1):
+        n = prices.popleft()
+        answer.append(n_prices-i-1)
+        for idx,j in enumerate(prices):
+            if n > j:
+                answer.pop()
+                answer.append(idx+1)
+                break
+        
+    answer.append(0)   
     return answer
