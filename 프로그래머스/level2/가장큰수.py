1eea4c03bbd5fcb36ade2612d307eca424d737f2
조수혜 <99079272+suhyehye@users.noreply.github.com>
2023-01-19 15:08:46 +0900

    Update 가장큰수.py

--- 프로그래머스/level2/가장큰수.py
+++ 프로그래머스/level2/가장큰수.py
@@ -1,5 +1,20 @@
-from itertools import permutations
+from collections import deque
 def solution(numbers):
-    answer = list(permutations(numbers,len(numbers)))
-    answer = max([''.join(map(str, x)) for x in answer])
-    return answer
+    numbers.sort(key=lambda x: (str(x)[0],str(x)*3,str(x)[-1]),reverse=True)
+    numbers = deque(numbers)
+    answer = ''
+    
+    while len(numbers) >= 2:
+        n1 = str(numbers.popleft())
+        n2 = str(numbers.popleft())
+        if len(n1) == 1:
+            answer += n1
+            numbers.appendleft(n2)
+        elif n1 + n2 >= n2 + n1:
+            answer += n1
+            numbers.appendleft(n2)
+        else:
+            numbers.appendleft(n1)
+            numbers.appendleft(n2)
+    answer += ''.join(numbers)
+    return str(int(answer))
