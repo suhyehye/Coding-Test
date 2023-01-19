15ee38acf7c2a203d51a575855122f11d815f3ed
조수혜 <99079272+suhyehye@users.noreply.github.com>
2023-01-19 15:12:14 +0900

    Update 가장큰수.py

--- 프로그래머스/level2/가장큰수.py
+++ 프로그래머스/level2/가장큰수.py
@@ -1,20 +1,4 @@
-from collections import deque
 def solution(numbers):
-    numbers.sort(key=lambda x: (str(x)[0],str(x)*3,str(x)[-1]),reverse=True)
-    numbers = deque(numbers)
-    answer = ''
+    numbers.sort(key=lambda x: str(x)*3,reverse=True)
     
-    while len(numbers) >= 2:
-        n1 = str(numbers.popleft())
-        n2 = str(numbers.popleft())
-        if len(n1) == 1:
-            answer += n1
-            numbers.appendleft(n2)
-        elif n1 + n2 >= n2 + n1:
-            answer += n1
-            numbers.appendleft(n2)
-        else:
-            numbers.appendleft(n1)
-            numbers.appendleft(n2)
-    answer += ''.join(numbers)
-    return str(int(answer))
+    return str(int(''.join(list(map(str,numbers)))))
