463771a52357cd1306d6a363ced92ff8412a7ac1
suhye <suhyehye@gmail.com>
2023-03-25 16:16:23 +0900

    change 8

--- kakao/다트게임.py
+++ kakao/다트게임.py
@@ -16,7 +16,6 @@ def solution(dart):
             answer[-1] = answer[-1]*2
             answer.append(eval(nums[i]+points[i][:-1]+'*2'))
             
-            
         else:
             answer.append(eval(nums[i]+points[i]))
     
