5d2135f6930e916b3b3e145205ac84c488b80fd8
조수혜 <99079272+suhyehye@users.noreply.github.com>
2023-04-13 11:02:25 +0900

    [Gold V] Title: 트리, Time: 68 ms, Memory: 34140 KB -BaekjoonHub

---
+++ 백준/Gold/1068. 트리/트리.py
@@ -0,0 +1,31 @@
+from collections import deque
+N = int(input())
+
+arr = list(map(int, input().split()))
+M = int(input())
+
+graph = [[] for _ in range(N+1)]
+for idx, x in enumerate(arr):
+    if x != -1:
+        graph[x].append(idx)
+
+
+def bfs(start):
+    arr[start] = -2
+    q = deque()
+    q.append(start)
+    
+    while q:
+        x = q.popleft()
+        for i in graph[x]:
+            if arr[i] != -2:
+                arr[i] = -2
+                q.append(i)
+
+bfs(M)
+
+cnt = 0
+for i in range(len(arr)):
+    if arr[i] != -2 and i not in arr:
+        cnt += 1
+print(cnt)
