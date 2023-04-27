0a33fbda121af54ff2aadc9668352ebf340d38a3
조수혜 <99079272+suhyehye@users.noreply.github.com>
2023-04-28 01:31:10 +0900

    [level 3] Title: 등굣길, Time: 1.63 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/42898. 등굣길/등굣길.py
@@ -0,0 +1,17 @@
+def solution(m, n, puddles):
+    dp = [[0 for _ in range(m+1)] for _ in range(n+1)]
+    dp[1][1] = 1
+
+    for p in puddles:
+        dp[p[1]][p[0]] = -1
+        
+    for i in range(1, n+1):
+        for j in range(1, m+1):
+        
+            if dp[i][j] == -1:
+                dp[i][j] = 0
+                continue
+            else:
+                dp[i][j] += dp[i-1][j] + dp[i][j-1]
+                
+    return dp[n][m] % 1000000007
