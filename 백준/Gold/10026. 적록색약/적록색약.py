8104f94b43fd2fab899fa0b8060c646599e360c7
조수혜 <99079272+suhyehye@users.noreply.github.com>
2023-08-18 14:25:27 +0900

    [Gold V] Title: 적록색약, Time: 92 ms, Memory: 34208 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.py
+++ 백준/Gold/10026. 적록색약/적록색약.py
@@ -1,50 +1,43 @@
-import sys
-sys.setrecursionlimit(1000000)
-
+from collections import deque
 N = int(input())
-
-array = []
-for _ in range(N):
-    array.append(list(input().rstrip()))
-    
+graph = [list(input().rstrip()) for _ in range(N)]
 visited = [[False] * N for _ in range(N)]
 
-dx = [1,-1,0,0]
-dy = [0,0,1,-1]
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
 
-def dfs(x,y):
-    visited[x][y] = True
-    color = array[x][y]
-    
-    for i in range(4):
-        new_x, new_y = x+dx[i], y+dy[i]
-        
-        if (0<= new_x < N) and (0 <= new_y < N):
-            if visited[new_x][new_y] == False and array[new_x][new_y] == color:
-                dfs(new_x, new_y)
+def bfs(x, y, color):
+    q = deque()
+    q.append([x,y])
+    while q:
+        x, y = q.popleft()
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if 0 <= nx < N and 0 <= ny < N and graph[nx][ny] == color and visited[nx][ny] == False : 
+                q.append([nx,ny])
+                visited[nx][ny] = True
 
-cnt1, cnt2 = 0,0
 
-# 색맹이 아닌 경우
-for x in range(N):
-    for y in range(N):
-        if visited[x][y] == False:
-            dfs(x,y)
-            cnt1 += 1
-            
-# 색맹인 경우 - visited 초기화
-visited = [[False] * N for _ in range(N)]
-
-# G-> R로 바꿔줌
+cnt1, cnt2 = 0, 0
 for i in range(N):
     for j in range(N):
-        if array[i][j] == 'G':
-            array[i][j] = 'R'
-    
-for x in range(N):
-    for y in range(N):
-        if visited[x][y] == False:
-            dfs(x,y)
-            cnt2 += 1
+        if visited[i][j] == False:
+            bfs(i,j,graph[i][j])
+            visited[i][j] = True
+            cnt1 += 1
 
+# Green을 Red로 변환 (적록색맹)
+for i in range(N):
+    for j in range(N):
+        if graph[i][j] == "G":
+            graph[i][j] = "R"
+
+visited = [[False] * N for _ in range(N)]
+for i in range(N):
+    for j in range(N):
+        if visited[i][j] == False:
+            bfs(i,j,graph[i][j])
+            visited[i][j] = True
+            cnt2 += 1
 print(cnt1, cnt2)
