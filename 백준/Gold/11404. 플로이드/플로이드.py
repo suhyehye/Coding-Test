9039ee6e252eb057416a6baee297e1df14584bcd
조수혜 <99079272+suhyehye@users.noreply.github.com>
2023-08-19 17:01:18 +0900

    [Gold IV] Title: 플로이드, Time: 4772 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Gold/11404. 플로이드/플로이드.py
+++ 백준/Gold/11404. 플로이드/플로이드.py
@@ -1,23 +1,24 @@
 import sys
-N = int(input())
-M = int(input())
 INF = sys.maxsize
 
-cost = [[INF] * N for _ in range(N)]
+N = int(input())
+M = int(input())
+
+dist = [[INF] * (N) for _ in range(N)]
 
 for _ in range(M):
     a, b, c = map(int, input().split())
-    cost[a-1][b-1] = min(cost[a-1][b-1],c)
-    
+    dist[a-1][b-1] = min(c, dist[a-1][b-1])
+            
 for i in range(N):
     for j in range(N):
         for k in range(N):
-            cost[j-1][k-1] = min(cost[j-1][k-1], cost[j-1][i-1]+cost[i-1][k-1])
-      
+            dist[j][k] = min(dist[j][k], dist[j][i] + dist[i][k])
+            
 for i in range(N):
     for j in range(N):
-        if i == j or cost[i-1][j-1] == INF:
-            cost[i-1][j-1] = 0
-    
-for arr in cost:
-    print(*arr)
+        if dist[i][j] == INF or i == j:
+            dist[i][j] = 0
+            
+for i in dist:
+    print(*i)
