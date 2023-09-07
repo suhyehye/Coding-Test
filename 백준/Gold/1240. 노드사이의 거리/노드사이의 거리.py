c5b7167d955e417fee1a6621d2bd9a561074538e
조수혜 <99079272+suhyehye@users.noreply.github.com>
2023-09-08 03:42:28 +0900

    [Gold V] Title: 노드사이의 거리, Time: 200 ms, Memory: 34176 KB -BaekjoonHub

--- 백준/Gold/1240. 노드사이의 거리/노드사이의 거리.py
+++ 백준/Gold/1240. 노드사이의 거리/노드사이의 거리.py
@@ -1,19 +1,31 @@
 import sys
+from collections import deque
 input = sys.stdin.readline
-N, M = map(int, input().split())
-graph = [[100000000] * N for _ in range(N)]
 
-for _ in range(N-1):
-    x, y, d = map(int, input().split())
-    graph[x-1][y-1] = d
-    graph[y-1][x-1] = d
+def bfs(start,find):
+    queue = deque()
+    queue.append((start,0))
+    visited = [False] * (n+1)
+    visited[start] = True
+    while queue:
+        v, d = queue.popleft()
+        
+        if v == find: #찾는 노드와 번호가 일치하면 거리 리턴
+            return d
+        
+        for i, l in graph[v]: #연관된 노드에 대한 노드번호와 간선길이
+            if not visited[i]:
+                visited[i] = True
+                queue.append((i,d+l)) #지금까지 노드를 찾으면서 거리를 기록
     
-for k in range(N):
-    for i in range(N):
-        for j in range(N):
-            if graph[i][j] > graph[i][k] + graph[k][j]:
-                graph[i][j] = graph[i][k] + graph[k][j]
-            
-for _ in range(M):
-    x, y = map(int, input().split())
-    print(graph[x-1][y-1])
+n, m = map(int,input().split())
+graph = [[] for _ in range(n+1)]
+
+for _ in range(n-1):
+    n1, n2, l = map(int,input().split())
+    graph[n1].append((n2,l))
+    graph[n2].append((n1,l))
+
+for _ in range(m):
+    n1, n2 = map(int,input().split())
+    print(bfs(n1,n2))
