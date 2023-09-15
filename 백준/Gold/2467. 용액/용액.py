72615212faf8a31f9300acdef7ef4848a653cace
조수혜 <99079272+suhyehye@users.noreply.github.com>
2023-09-15 14:07:23 +0900

    [Gold V] Title: 용액, Time: 132 ms, Memory: 42172 KB -BaekjoonHub

--- 백준/Gold/2467. 용액/용액.py
+++ 백준/Gold/2467. 용액/용액.py
@@ -1,35 +1,21 @@
 import sys
-
 N = int(input())
 arr = list(map(int, input().split()))
-
 arr.sort()
 
-left = 0
-right = N-1
-
-# 두 값을 더한 값
-x = sys.maxsize
-answer = []
-
-while left < right:
-    left_ = arr[left]
-    right_ = arr[right]
+x1, x2 = 0, N-1
+total = sys.maxsize
+ans = []
+while x1 < x2:
+    temp = arr[x1] + arr[x2]
     
-    total = left_ + right_
-    
-    # 0에 더 가까운 값을 정답 리스트에 추가함
-    if abs(total) < x:
-        answer = [left_, right_]
-        x = abs(total)
-        
-    # 더한 값이 0보다 작으면 왼쪽 값을 늘려 0에 가깝게
-    if total < 0:
-        left += 1
-    elif total > 0:
-        right -= 1
-        
+    if abs(temp) < total:
+        total = abs(temp)
+        ans = [arr[x1], arr[x2]]
+    if temp > 0:
+        x2 -= 1
+    elif temp < 0:
+        x1 += 1
     else:
         break
-
-print(answer[0], answer[1])
+print(*ans)
