ffc1f9d43889ce2b6c87c84b2ab02875b729d64a
조수혜 <99079272+suhyehye@users.noreply.github.com>
2023-11-07 23:14:56 +0900

    [Gold V] Title: 숫자고르기, Time: 68 ms, Memory: 34068 KB -BaekjoonHub

---
+++ 백준/Gold/2668. 숫자고르기/숫자고르기.py
@@ -0,0 +1,32 @@
+import sys
+from collections import defaultdict
+input = sys.stdin.readline
+
+n = int(input())
+data = defaultdict(list)
+
+for i in range(1, n+1):
+    data[int(input())].append(i)
+
+visited = [0] * (n+1)
+ans  = []
+
+def dfs(x, tmp):
+    visited[x] = 1
+    tmp.add(x)
+    
+    for i in data[x]:
+        if i not in tmp:
+            dfs(i, tmp.copy())
+
+        else:
+            ans.extend(list(tmp))
+
+for i in range(1, n+1):
+    if not visited[i]:
+        dfs(i, set([]))
+
+ans.sort()                
+print(len(ans))
+for i in ans:
+    print(i)
