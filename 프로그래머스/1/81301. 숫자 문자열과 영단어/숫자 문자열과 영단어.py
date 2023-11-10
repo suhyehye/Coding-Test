a95ba8cc683cd36ccb1db987b53069a27218ac7c
조수혜 <99079272+suhyehye@users.noreply.github.com>
2023-11-11 01:33:18 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.02 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
@@ -1,19 +1,9 @@
 def solution(s):
-    res = ""
     digit = {"zero" : "0", "one": "1", "two": "2",
             "three" : "3", "four" : "4", "five" : "5",
             "six" : "6", "seven" : "7", "eight" : "8",
             "nine" : "9"}
     
-    tmp = ""
-    for i in s:
-        if i.isdigit():
-            res += i
-            continue
-            
-        tmp += i
-        if tmp in digit.keys():
-            res += digit[tmp]
-            tmp = ""
-            
-    return int(res)
+    for key, v in digit.items():
+        s = s.replace(key, v)
+    return int(s)
