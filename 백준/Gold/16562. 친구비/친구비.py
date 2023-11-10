dd4308ee304f76852ac67db74c8f2f8c0891e307
조수혜 <99079272+suhyehye@users.noreply.github.com>
2023-11-10 15:12:07 +0900

    [Gold IV] Title: 친구비, Time: 80 ms, Memory: 34088 KB -BaekjoonHub

--- 백준/Gold/16562. 친구비/친구비.py
+++ 백준/Gold/16562. 친구비/친구비.py
@@ -3,39 +3,34 @@ from collections import deque
 input = sys.stdin.readline
 
 n, m, k = map(int, input().split())
-cost = list(map(int, input().split()))
-graph = [set() for _ in range(n)]
-visited = [0 for _ in range(n)]
+cost = [0] + list(map(int, input().split()))
+parents = [x for x in range(n+1)]
 
-for _ in range(m):
-    a, b = map(int, input().split())
-    if a == b:
-        continue
-    graph[a-1].add(b-1)
-    graph[b-1].add(a-1)
+def find(x: int) -> int:
+    if parents[x] == x:
+        return x
+    parents[x] = find(parents[x])
+    return parents[x]
 
-def bfs(x):
-    visited[x] = 1
-    q = deque()
-    q.append(x)
-    tmp = set([x])
+def union(x:int, y:int) -> None:
+    x, y = find(x), find(y)
     
-    while q:
-        x = q.popleft()
-        for i in graph[x]:
-            if not visited[i]:
-                tmp.add(i)
-                q.append(i)
-                visited[i] = 1
-    
-    c_tmp = cost[x]
-    for i in tmp:
-        c_tmp = min(c_tmp, cost[i])
-    return c_tmp
-
-ans = 0
-for i in range(n):
-    if not visited[i]:
-        ans += bfs(i)
+    if cost[x] < cost[y]:
+        parents[y] = x
+    else:
+        parents[x] = y
         
+        
+for _ in range(m):
+    v, w = map(int, input().split())
+    union(v, w)
+        
+friends = set()    
+ans = 0
+
+for i in range(1, n+1):
+    if find(i) not in friends:
+        ans += cost[parents[i]]
+        friends.add(parents[i])
+
 print(ans) if ans <= k else print("Oh no")
