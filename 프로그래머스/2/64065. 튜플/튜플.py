6145a80bf78b982f10f1e84c52ae303793c9c1c6
조수혜 <99079272+suhyehye@users.noreply.github.com>
2023-11-12 02:31:52 +0900

    [level 2] Title: 튜플, Time: 0.14 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/튜플.py
+++ 프로그래머스/2/64065. 튜플/튜플.py
@@ -1,11 +1,5 @@
 import re
+from collections import Counter
 def solution(s):
-    answer = []
-    s = re.split(r"{|}", s)
-    s = [re.findall(r"\d+", x) for x in s if x!="" and x != ","]
-    s = sorted(s, key = lambda x:len(x))
-    for x in s:
-        for y in x:
-            if int(y) not in answer:
-                answer.append(int(y))
-    return answer
+    count = Counter(re.findall(r"\d+", s))
+    return [int(k) for k,v in sorted(count.items(), key=lambda x:x[1], reverse=True)]
