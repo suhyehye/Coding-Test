54c87f4926e3a0afe00175f60d960f2e949573eb
조수혜 <99079272+suhyehye@users.noreply.github.com>
2023-11-16 16:19:06 +0900

    [level 1] Title: 신규 아이디 추천, Time: 0.22 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.py
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.py
@@ -9,9 +9,6 @@ def solution(new_id):
     new_id = new_id[:15]
     new_id = new_id.strip('.')
     if len(new_id) <= 2:
-        if not new_id:
-            x = 'a'
-        else:
-            x = new_id[-1]
+        x = new_id[-1]
         new_id = new_id + x * (3-len(new_id))
     return new_id
