e4319678f2140e2de46aa569a1209daf8efff7cc
조수혜 <99079272+suhyehye@users.noreply.github.com>
2023-11-16 16:17:59 +0900

    [level 1] Title: 신규 아이디 추천, Time: 0.15 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.py
@@ -0,0 +1,17 @@
+import re
+def solution(new_id):
+    new_id = new_id.lower()
+    new_id = re.sub(r'[^a-z0-9-_\.]', '', new_id)
+    new_id = re.sub(r'\.+', '.', new_id)
+    new_id = new_id.strip('.')
+    if not new_id:
+        new_id = 'aaa'
+    new_id = new_id[:15]
+    new_id = new_id.strip('.')
+    if len(new_id) <= 2:
+        if not new_id:
+            x = 'a'
+        else:
+            x = new_id[-1]
+        new_id = new_id + x * (3-len(new_id))
+    return new_id
