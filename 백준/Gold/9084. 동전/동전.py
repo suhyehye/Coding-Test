a1b27a876fa1e132bb80dc85afac1a6c8d01185d
조수혜 <99079272+suhyehye@users.noreply.github.com>
2023-11-21 19:37:56 +0900

    [Gold V] Title: 동전, Time: 72 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Gold/9084. 동전/동전.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+
+t =  int(input())
+for _ in range(t):
+    n = int(input())
+    coins = list(map(int, input().split()))
+    m = int(input())
+    dp = [0] * (m+1)
+    dp[0] = 1
+    
+    for coin in coins:
+        for i in range(coin, m+1):
+            dp[i] += dp[i - coin]
+    print(dp[-1])
