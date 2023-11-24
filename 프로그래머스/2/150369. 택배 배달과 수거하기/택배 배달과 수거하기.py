3ed8e1e010219dc41edf5ad774f17650d3f959dd
조수혜 <99079272+suhyehye@users.noreply.github.com>
2023-11-24 22:59:56 +0900

    [level 2] Title: 택배 배달과 수거하기, Time: 80.20 ms, Memory: 11.7 MB -BaekjoonHub

--- 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.py
+++ 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.py
@@ -1,6 +1,19 @@
+def remove(arr, cap):
+    tmp = 0
+    for i in range(len(arr)-1, -1, -1):
+        if arr[i] + tmp <= cap:
+            tmp += arr[i]
+            arr.pop()
+            if i == 0:
+                break
+        else:
+            if cap - tmp > 0:
+                arr[i] -= cap - tmp
+            break
+    return arr
+    
 def solution(cap, n, deliveries, pickups):
     answer = 0
-    tmp = 0
     while deliveries or pickups:
         if deliveries and deliveries[-1] == 0:
             deliveries.pop()
@@ -11,30 +24,6 @@ def solution(cap, n, deliveries, pickups):
         
         answer += max(len(deliveries), len(pickups)) * 2
         
-        for i in range(len(deliveries)-1, -1, -1):
-            if deliveries[i] + tmp <= cap:
-                tmp += deliveries[i]
-                deliveries.pop()
-                if i == 0:
-                    break
-            else:
-                if cap - tmp > 0:
-                    deliveries[i] -= cap - tmp
-                break
-                
-
-        tmp = 0
-        
-        for i in range(len(pickups)-1, -1, -1):
-            if pickups[i] + tmp <= cap:
-                tmp += pickups[i]
-                pickups.pop()
-                if i == 0:
-                    break
-            else:
-                if cap - tmp > 0:
-                    pickups[i] -= cap - tmp
-                break
-        tmp = 0
-    
+        if deliveries : deliveries = remove(deliveries, cap)
+        if pickups : pickups = remove(pickups, cap)
     return answer
